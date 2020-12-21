4561cc7fee94f31a90b5ebdb47ef35dd8f5ff4c1
EDGAR JAVIER QUISPE MAMANI <ed.quispe.mamani@isur.edu.pe>
2020-12-20 21:53:06 -0500

    gestion de productos

---
+++ App/gestion_productos.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace App
+{
+    partial class gestion_productos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.codigo_producto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "codigo_producto";
+            // 
+            // codigo_producto
+            // 
+            this.codigo_producto.Location = new System.Drawing.Point(36, 82);
+            this.codigo_producto.Name = "codigo_producto";
+            this.codigo_producto.Size = new System.Drawing.Size(100, 20);
+            this.codigo_producto.TabIndex = 1;
+            this.codigo_producto.Text = " ";
+            // 
+            // gestion_productos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.codigo_producto);
+            this.Controls.Add(this.label1);
+            this.Name = "gestion_productos";
+            this.Text = "gestion_productos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox codigo_producto;
+    }
+}
