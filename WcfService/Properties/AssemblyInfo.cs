188a8b19424b4449aac2a0d68589654429cb866f
EDGAR JAVIER QUISPE MAMANI <ed.quispe.mamani@isur.edu.pe>
2021-01-09 11:53:26 -0500

    gestion productos

--- WcfService/Properties/AssemblyInfo.cs
+++ WcfService/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aeab8b42-3a41-4d35-a2f7-d7d5b5fca275")]
+[assembly: Guid("e6f65adc-a8fe-4c2f-9d61-cce4627a26f8")]
 
 // Version information for an assembly consists of the following four values:
 //
